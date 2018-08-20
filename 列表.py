71529acdd3a20503409b53da235ca4c11779def1
Johnhao <x_johnhao@163.com>
2018-08-20 22:33:59 +0800

    列表

---
+++ 列表.py
@@ -0,0 +1,8 @@
+a = ['spam', 'eggs', 100, 120]
+print(a)
+squares = [1, 3, 4, 5, 7, 8]
+print(len(squares))
+print(squares[0])
+print(squares[-1])
+print(squares[-3:])
+print(squares[:])
---
+++ 字符串.py
@@ -0,0 +1,12 @@
+print('spam eggs')
+print('doesn\'t')
+print("doesn't")
+print('"yes," he said')
+print("\"yes,\" he said.")
+print('"isn\'t," she said')
+
+word = "Help" + "A"
+print('<' + word * 5 + '>')
+print(word[4])
+print(word[0:2])
+print(word[2:])
